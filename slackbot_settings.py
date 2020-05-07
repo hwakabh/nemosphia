eb977b501fa2a9f16c229eedc33dd589aa2d41a4
hwakabh <hrykwkbys1024@gmail.com>
2020-05-07 15:13:45 +0900

    Updated precheck for environmental variables.

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -2,21 +2,17 @@ import os
 import sys
 
 API_TOKEN = os.environ.get('API_TOKEN')
-if API_TOKEN == None:
-    print('>>> No API_TOKEN parameter configured, please use `export API_TOKEN="YOUR_TOKEN"` commands first.')
-    sys.exit('>>> Bot programs failed to run, exit...')
 
-# Get vSphere credentials securely from environmental variables
-VC_IPADDR = os.environ.get('VSPHERE_IP')
-VC_USERNAME = os.environ.get('VSPHERE_USERNAME')
-VC_PASSWORD = os.environ.get('VSPHERE_PASSWORD')
-
-if (VC_IPADDR is None) or (VC_USERNAME is None) or (VC_PASSWORD is None):
-    print('>>> No vSphere credentials provided. Confirm the parameters have been configured properly with environmental variables like:')
-    print('\t export VSPHERE_IP=\'vcsa01.mylab.local\'')
-    print('\t export VSPHERE_USERNAME=\'administrator@vsphere.local\'')
-    print('\t export VSPHERE_PASSWORD=\'VMware1!\'')
-    sys.exit('>>> Bot programs failed to run, exit...')
+# Base authentication with prechecking
+print('>>> Starting slackbot, wait for a while to initializing...')
+print('--->>> Prechecking base requirements')
+if API_TOKEN is None:
+    print('Failed to retrive API_TOKEN for slackbot.')
+    print('Please set API_TOKEN as environmental variables on your runtime.')
+    print('    export API_TOKEN=\'YOUR_TOKEN_PUBLISH_BY_SLACK_ADMIN\'')
+    sys.exit(1)
+else:
+    print('done')
 
 # DEFAULT_REPLY = "Hello World from SlackBot !!"
 PLUGINS = ['plugins']
